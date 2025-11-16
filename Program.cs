96487f2beda8154fa0c9efa06e92aa9fe04d85ed
Aline Gpp <alinegoppinger@gmail.com>
2025-11-16 16:57:24 -0300

    first commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero:"54982435167",modelo:"111111111",Imei:"76386237",memoria:64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+
+Console.WriteLine("Smartphone iPhone");
+Smartphone iphone = new Iphone(numero:"54982435167",modelo:"111111111",Imei:"76386237",memoria:64);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
+
+
